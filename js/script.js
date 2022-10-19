971c0b6c86a5ee44c6c66d351fb183a30a89665b
AndreaMatarrese <matarreseandrea47@gmail.com>
2022-10-19 16:51:07 +0200

    Completed

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito22
+
+let nome = prompt('Qual è il tuo nome?')
+let cognome = prompt('Qual è il tuo cognome?')
+let colorePreferito =prompt('Qual è il tuo colore preferito?')
+
+console.log('generatore password sicure')
+document.getElementById('password').innerHTML= (`${nome}${cognome}${colorePreferito}22`)
